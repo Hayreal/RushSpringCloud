bd0925122ccca5e3b7bb1b2f8b94ae9e9f6b3147
unknown <1135564544@qq.com>
2021-01-03 22:26:41 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,34 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @PostMapping("/Payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int result = paymentService.create(payment);
+        if (result>0){
+            return new CommonResult(200,"插入数据成功",payment);
+        }else {
+            return new CommonResult(444,"插入失败",payment);
+        }
+    }
+    @GetMapping("/Payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        if (payment != null){
+            return new CommonResult(200,"查询成功",payment);
+        }else {
+            return new CommonResult(400,"查询失败",payment);
+        }
+    }
+}
